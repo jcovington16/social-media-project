35d18512b9e7cac0a6dd5fdd97ac18ec962bea60
jcovington16 <covingtonjoshua7@gmail.com>
2021-10-26 17:33:44 -0600

    Adding the frontend back to the project.

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import {Switch, Route} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        
+      </Switch>
+
+    </div>
+  );
+}
+
+export default App;
