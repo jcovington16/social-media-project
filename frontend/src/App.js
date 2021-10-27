768dc685f0276230d4880e7c7130f3c9726f6ae6
jcovington16 <covingtonjoshua7@gmail.com>
2021-10-27 16:48:17 -0600

    Adding jwtdecode and setting up our ability to render screens based on if our user is logged in or not.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import {Switch, Route} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
+import jwtDecode from 'jwt-decode';
+
+
+
 
 function App() {
+
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    const jwt = localStorage.getItem('token');
+    try{
+      setUser(jwtDecode(jwt));
+  
+    } catch {
+  
+    }
+  },[])
+
   return (
     <div className="App">
       <Switch>
