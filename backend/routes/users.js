12548619524c4ae33b71e5d2da546bed97b088de
martinharts <89169036+martinharts@users.noreply.github.com>
2021-11-02 19:27:28 -0400

    added code to user rout for sending friend request. testing code

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -47,4 +47,28 @@ router.post('/', async (req, res) => {
     }
    });
 
+   //send friend request
+
+   router.put("/:_id/follow", async (req, res) => {
+       if(req.body.userId !== req.params._id){
+           try{
+               const user = await User.findById(req.params._id);
+               const currentUser = await User.findById(req.body.userId);
+               if(!user.followers.includes(req.body.userId)){
+                    await user.updateOne({$push: {listOfRequests: req.body.userId}});
+                    await currentUser.updateOne({$push: {listOfFriends: req.body.userId}});
+                    res.status(200).json("user has sent friend request");
+               }else{
+                   res.status(403).json('you are already friends with this user')
+               }
+           }catch(err){
+               res.status(500).json(err);
+           }
+       }else{
+           res.status(403).json("you can't send yourself a friend request");
+       }
+   });
+
+   //
+
 module.exports = router;
