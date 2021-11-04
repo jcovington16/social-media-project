561e2377ca8f77c90fb4a29fb23c5bcca57d7a18
jcovington16 <covingtonjoshua7@gmail.com>
2021-11-03 19:06:42 -0600

    We now have the ability to remove a user from multiple friends list.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const auth = require('../middleware/auth');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const { request } = require('express');
 
 
 router.get('/', async (req,res) => {
@@ -56,39 +57,38 @@ router.post('/', async (req, res) => {
 
                if(!user.friends.includes(req.body.userId)){
                     await user.updateOne({$push: {friendRequests: req.body.userId}});
-
-                    res.status(200).json("user has sent friend request");
-               }if(user.friends.includes(friendRequest[userId])){
-                   res.status(403).json("you already sent a friend request to user")
+                    return res.status(200).json("user has sent friend request");
+               }else if(user.friendRequests.includes(req.body.userId)){
+                   return res.status(403).json("you already sent a friend request to user")
                }
                else{
-                   res.status(403).json('you are already friends with this user')
+                   return res.status(403).json('you are already friends with this user')
                }
            }catch(err){
-               res.status(500).json(err);
+               return res.status(500).json(`Error: ${err}`);
            }
        }else{
-           res.status(403).json("you can't send yourself a friend request");
+           return res.status(403).json("you can't send yourself a friend request");
        }
    });
 
    //accept friend request
-   router.put("/:_id/friends", async (req, res) => {
+   router.put("/:_id/accept", async (req, res) => {
        if(req.body.userId !== req.params._id){
            try{
                const user = await User.findById(req.params._id);
                const requester = await User.findById(req.body.userId);
                if(!user.friends.includes(req.body.userId)){
                    await user.updateOne({$push: {friends: req.body.userId}});
-                   await user.updateOne({$pull: {friendRequest: req.body.userId}});
-                   await requester.updateOne({$push: {friends: req.body.userId}});
-                   res.status(200).json("accepted friend request")
+                   await user.updateOne({$pull: {friendRequests: req.body.userId}});
+                   await requester.updateOne({$push: {friends: req.params._id}});
+                   return res.status(200).json("accepted friend request")
                }
                else{
-                   res.status(403).json("you are already friends");
+                   return res.status(403).json("you are already friends");
                }
            }catch(err){
-               res.status(500).json(err);
+               return res.status(500).json(`Error: ${err}`);
            }
        }
    });
@@ -103,13 +103,13 @@ router.post('/', async (req, res) => {
                if(user.friends.includes(req.body.userId)){
                 await user.updateOne({$pull: {friends: req.body.userId}});
                 await friend.updateOne({$pull: {friends: req.params._id}});
-                res.status(200).json("You've successfully removed friend");
+                return res.status(200).json("You've successfully removed friend");
                } else {
-                   res.status(403).json("User is not in your friends list.");
+                   return res.status(403).json("User is not in your friends list.");
                }
 
            } catch(err) {
-               res.status(500).json(err);
+               return res.status(500).json(err);
            }
        }
    })
@@ -121,13 +121,13 @@ router.post('/', async (req, res) => {
 
                if(user.friendRequests.includes(req.body.userId)) {
                    await user.updateOne({$pull: {friendRequests: req.body.userId}})
-                   res.status(200).json("You've successfully removed request");
+                   return res.status(200).json("You've successfully removed request");
                } else {
-                   res.status(403).json("User is not in your pending request");
+                   return res.status(403).json("User is not in your pending request");
                }
 
            } catch(err) {
-               res.status(500).json(err);
+               return res.status(500).json(err);
            }
        }
    })
